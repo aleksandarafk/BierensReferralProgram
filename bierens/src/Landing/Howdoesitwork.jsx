4f7dbb8b4523a2e9ce89c912179ceb40b8306560
Deniz Hadjiosmanov <d.hadjiosmanov@student.fontys.nl>
2024-05-25 13:46:03 +0200

    Made the layouting of the How Does it Work section from the Landing page

---
+++ bierens/src/Landing/Howdoesitwork.jsx
@@ -0,0 +1,33 @@
+import './style.css'
+
+function Howdoesitwork () {
+    return (
+      <div id='section2_container'>
+          <p> How does it work? </p>
+          <p> Bierens referral program strives to make it easier for you, enhancing transparency in the referral process </p>
+          <div id='cards_container'>
+              <div className='cards_row'>
+                  <div>
+                   <p>Hey hey hey!</p>
+                  </div>
+
+                  <div>
+                  <p>Hey hey hey!</p>
+                  </div>
+              </div>
+              
+              <div className='cards_row'>
+              <div>
+                   <p>Hey hey hey!</p>
+                  </div>
+
+                  <div>
+                  <p>Hey hey hey!</p>
+                  </div>
+              </div>      
+          </div>
+      </div>  
+    );
+}
+
+export default Howdoesitwork;
--- bierens/src/Landing/Landing.jsx
+++ bierens/src/Landing/Landing.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Howdoesitwork from "./Howdoesitwork";
+import './style.css'
 
 const Landing = () => {
-    return(<div>
-
-    <h1> Landing Page </h1>
-        </div>)
+    return(
+        <div>
+        <Howdoesitwork></Howdoesitwork>
+        </div>
+    )
 }
 
 export default Landing;
