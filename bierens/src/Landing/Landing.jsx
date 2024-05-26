cd756406f2f4cdab977227d6f0244be6b8d0373d
Ivan Petrov <i.petrov@simptel.com>
2024-05-26 13:57:45 +0200

    creating layout of the left side of the APPLY section.

--- bierens/src/Landing/Landing.jsx
+++ bierens/src/Landing/Landing.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
 const Landing = () => {
-    return(<div>
+    return(
+        <section className="How-to-apply">
 
-    <h1> Landing Page </h1>
-        </div>)
+            <div className="How-to-apply-text">
+                <h1>Refer,<h1 className="Earn"> Earn,</h1>Repeat.</h1>
+                <p>Apply today for the chance of being accepted into our B2B referral program. Earn special perks and rewards based on your referral performance.</p>
+                <input type="text" className="email" name="email" placeholder="example@bierens.com" ></input>
+                <button> Apply</button>
+
+
+
+            </div>
+
+
+
+
+        </section>
+        )
 }
 
 export default Landing;
