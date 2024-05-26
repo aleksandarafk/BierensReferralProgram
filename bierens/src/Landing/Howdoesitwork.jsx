41f65b4c07ca807edf4ffe7d3dc29e8a4137458e
Deniz Hadjiosmanov <d.hadjiosmanov@student.fontys.nl>
2024-05-26 20:42:42 +0200

    Added content of the boxes and adjusted its structure. Included the correct fonts

--- bierens/src/Landing/Howdoesitwork.jsx
+++ bierens/src/Landing/Howdoesitwork.jsx
@@ -3,26 +3,52 @@ import './style.css'
 function Howdoesitwork () {
     return (
       <div id='section2_container'>
-          <p> How does it work? </p>
-          <p> Bierens referral program strives to make it easier for you, enhancing transparency in the referral process </p>
+          <p id='section2_title'> How does it <c>work?</c> </p>
+          <p id='section2_description'> Bierens referral program strives to make it easier for you, enhancing transparency in the referral process </p>
+          
           <div id='cards_container'>
               <div className='cards_row'>
-                  <div>
-                   <p>Hey hey hey!</p>
+                  <div className='small_card'>
+                      <div className='card_description'>
+                          <div className='bullet_number'>
+                            <p>1</p>
+                          </div>
+                          <p className='card_title'>Sign up and share</p>
+                          <p>Sign up and share your unique referral URL.</p>
+                      </div>
+
                   </div>
 
-                  <div>
-                  <p>Hey hey hey!</p>
+                  <div className='big_card'>
+                      <div className='card_description'>
+                          <div className='bullet_number'>
+                            <p>2</p>
+                          </div>
+                          <p className='card_title'>We qualify your lead</p>
+                          <p>We will qualify your referral to make sure they can benefit from us.</p>
+                      </div>
                   </div>
               </div>
               
               <div className='cards_row'>
-              <div>
-                   <p>Hey hey hey!</p>
+                  <div className='big_card'>
+                      <div className='card_description'>
+                          <div className='bullet_number'>
+                            <p>3</p>
+                          </div>
+                          <p className='card_title'>You earn rewards</p>
+                          <p>Once they sign up, you will get rewarded.</p>
+                      </div>
                   </div>
 
-                  <div>
-                  <p>Hey hey hey!</p>
+                  <div className='small_card'>
+                      <div className='card_description'>
+                          <div className='bullet_number'>
+                            <p>4</p>
+                          </div>
+                          <p className='card_title'>Earn bonus rewards</p>
+                          <p>Submit more referrals to reach milestones and earn exclusive bonus rewards.</p>
+                      </div>
                   </div>
               </div>      
           </div>
