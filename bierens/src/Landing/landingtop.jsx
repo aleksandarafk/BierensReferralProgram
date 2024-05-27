306eb592c5683891a04dcbb37ce1c78355bd2ed3
Chris Kit <chris@Chriss-Air.home>
2024-05-27 22:53:07 +0200

    re coded the site to look more like figma design

--- bierens/src/Landing/landingtop.jsx
+++ bierens/src/Landing/landingtop.jsx
@@ -9,13 +9,16 @@ function Landingtop () {
             <main>
                 <div id="main-inner">
                     <div class="main-inner-content" id="main-inner-left">
-                    <h1 class="landing-h1">Grow your <a class="highlight">network</a>.
-                        Earn better <a class="highlight">rewards</a>.</h1>
-                    <p id="landing-top-text">Grow your network with Bierens, access exclusive events, earn unique rewards for each referral trough our referral program. Become a Bierens Associate today.</p>
-                        <a href="#"><button class="button-calltoaction">Read more</button></a>
+                        <div id="main-inner-left-inner">
+                            <h1 class="landing-h1">Grow your <a class="highlight">network</a>.<br /> Earn better <a class="highlight">rewards</a>.</h1>
+                            <p id="landing-top-text">Grow your network with Bierens, access exclusive events, earn unique rewards for each referral trough our referral program. Become a Bierens Associate today.</p>
+                            <button class="button-calltoaction">Read more</button>
+                        </div>
                     </div>
                     <div class="main-inner-content" id="main-inner-right">
-                    <img src={introimage} id="landing-top-image" />
+                        <div id="main-inner-right-inner">
+                            <img src={introimage} id="landing-top-image" />
+                        </div>
                     </div>
                 </div>
             </main>
