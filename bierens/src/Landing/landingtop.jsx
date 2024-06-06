ebfb42416dab88aec29daf25d715a8242bdc31f6
Ivan Petrov <i.petrov@simptel.com>
2024-06-06 13:21:51 +0200

    Adding a scroll to section + Fixing the padding of Apply section

--- bierens/src/Landing/landingtop.jsx
+++ bierens/src/Landing/landingtop.jsx
@@ -1,29 +1,32 @@
-import React from  "react";
+import React from "react";
 import "./landingtop.css";
-import "./Landing-apply.css";
 import introimage from "./image/LandingPage-image.png";
 
-function Landingtop () {
-    return (
-        <>
-        <div className="Top-Landing">
-            <div className="Top-Landing-text">
-                <h1>
-                Grow your <span className="network">network. </span> <br/> Earn better <span className="network">rewards. </span>
-                </h1>
-                <p>
-                Grow your network with Bierens, access exclusive events, earn unique rewards for each referral trough our referral program. Become a Bierens Associate today.
-                </p>
-                <br/>
-                <button class="button-calltoaction">Read more</button>
-            </div>
-            <div>
-            <img className="RightImgTop" src={introimage} alt="img" />
-            </div>
-        
+function Landingtop() {
+  const scrollToSection = () => {
+    const section = document.getElementById('section2_container');
+    section.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <div className="Top-Landing">
+        <div className="Top-Landing-text">
+          <h1>
+            Grow your <span className="network">network. </span> <br /> Earn better <span className="network">rewards. </span>
+          </h1>
+          <p>
+            Grow your network with Bierens, access exclusive events, earn unique rewards for each referral through our referral program. Become a Bierens Associate today.
+          </p>
+          <br />
+          <button className="button-calltoaction" onClick={scrollToSection}>Read more</button>
+        </div>
+        <div>
+          <img className="RightImgTop" src={introimage} alt="img" />
+        </div>
       </div>
-      </>
-    )
+    </>
+  );
 }
 
 export default Landingtop;
